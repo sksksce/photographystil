2c95e240a42861275bde6f10ac875b1cdec931ab
golden7170 <93039819+golden7170@users.noreply.github.com>
2022-06-21 14:32:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ var count=document.getElementById('count');
  number++;
   count.innerHTML = number;
 }
+const press= document.getElementById('press');
+  press.addEventListener('click', function onClick(){
+  
+ press.textContent = 'FACK YOU DIP SHIFTY';
+ press.style.backgroundColor = 'yellow';
+  
+
+});
